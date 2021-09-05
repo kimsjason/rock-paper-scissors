7cb9e90a674ffe624e07f5bd5e0058beca77cdeb
Jason Kim <jason.s.kim95@gmail.com>
2021-09-05 01:45:22 -0400

    Add playRound function with rock paper scissors ruleset

--- main.js
+++
@@ -1,4 +0,0 @@
-let choices = ['rock', 'paper', 'scissors'];
-let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-console.log(computerChoice);
-let userChoice = prompt('Make your move (rock, paper, or scissors): ');
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,37 @@
 let choices = ['rock', 'paper', 'scissors'];
-let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-console.log(computerChoice);
+
+function computerPlay() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            return 'You tied!';
+        } else if (computerSelection == 'paper') {
+            return 'You lose! Paper beats rock.'
+        } else if (computerSelection == 'scissors') {
+            return 'You win! Rock beats scissors.';
+        }
+    } else if (playerSelection == 'paper') {
+            if (computerSelection == 'paper') {
+                return 'You tied!';
+            } else if (computerSelection == 'scissors') {
+                return 'You lose! Scissors beats paper.';
+            } else if (computerSelection == 'rock') {
+                return 'You win! Paper beats rock.';
+            }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'scissors') {
+            return 'You tied!';
+        } else if (computerSelection == 'rock') {
+            return 'You lose! Rock beats scissors.'
+        } else if (computerSelection == 'paper') {
+            return 'You win! Scissors beats paper.';
+        }
+    }
+
+}
+const playerSelection = prompt('Make your move (rock, paper, or scissors): ');
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
